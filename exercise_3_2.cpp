d020340cb8cc66d42a163d0c1e4ed53a6a51787b
John Lu <zhuolutetium@gmail.com>
2020-10-09 16:42:41 +0000

    finished

--- exercise_3_2.cpp
+++ exercise_3_2.cpp
@@ -6,25 +6,80 @@ LinkedList::LinkedList() {
 }
 
 void LinkedList::add_to_front(std::string name) {
+    std::unique_ptr<Node> newNode = std::make_unique<Node>();
+    newNode -> name = name;
+    newNode -> next = std::move(head);
+    head = std::move(newNode);
     return;
 }
 
-void LinkedList::add_to_back(std::string name){
+void LinkedList::add_to_back(std::string name) {
+	std::unique_ptr<Node> newNode = std::make_unique<Node>();
+    newNode -> name = name;
+    newNode -> next = nullptr;
+    if(head == nullptr) {
+    	head = std::move(newNode);
+    	return;
+    }
+    Node* curr = head.get();
+    while(curr -> next != nullptr) {
+    	curr = curr -> next.get();
+    }
+    curr -> next = std::move(newNode);
+
     return;
 }
 
 void LinkedList::add_at_index(std::string name, int index) {
+	std::unique_ptr<Node> newNode = std::make_unique<Node>();
+    newNode -> name = name;
+    newNode -> next = nullptr;
+    if(head == nullptr || index <= 0) {
+    	add_to_front(name);
+    	return;
+    }
+    Node* curr = head.get();
+    for(int i = 0; i < index - 1;  i++) {
+    	if(curr -> next == nullptr){
+    		curr -> next = std::move(newNode);
+    		return;
+    	}
+    	curr = curr -> next.get();
+    }
+    newNode->next = std::move(curr->next);
+    curr->next = std::move(newNode);
     return;
 }
 
 void LinkedList::remove_from_front() {
+	if(head != nullptr) {
+		head = std::move(head->next);
+	}
     return;
 }
 
 void LinkedList::remove_from_back() {
+    Node* curr = head.get();
+    if(head == nullptr) {
+    	return;
+    } else if (head ->next == nullptr) {
+    	head = std::move(nullptr);
+    	return;
+
+    }
+	while(curr->next->next != nullptr) {
+		curr = curr->next.get();
+	}
+	curr -> next = nullptr;
     return;
 }
 
 void LinkedList::print_names() {
+	Node* curr = head.get();
+	while(curr != nullptr) {
+		std::cout << curr->name << " ";
+		curr = curr->next.get();
+	}
+	std::cout << std::endl;
     return;
 }
--- main.cpp
+++ main.cpp
@@ -4,26 +4,29 @@
 
 int main() {
     LinkedList trainers = LinkedList();
+    trainers.add_to_back("test1");
+
+    trainers.add_to_front("test2");
+    trainers.add_to_back("test0");
+    trainers.add_at_index("test3", 0);
+
+    trainers.print_names();
+    trainers.remove_from_front();
+    trainers.print_names();
+
     trainers.remove_from_back();
-    trainers.add_to_front("Kyle");
-    trainers.print_names();
-    trainers.add_at_index("Daniel", 0);
-    trainers.print_names();
-    trainers.add_to_front("Hussain");
-    trainers.print_names();
-    trainers.remove_at_index(1);
-    trainers.print_names();
-    trainers.add_to_back("Oswin");
-    trainers.print_names();
-    trainers.add_at_index("Jason", 2);
+
     trainers.print_names();
     trainers.remove_from_back();
     trainers.print_names();
-    trainers.add_to_back("Woodward");
-    trainers.print_names();
-    trainers.remove_at_index(0);
+
+    trainers.remove_from_back();
     trainers.print_names();
 
 
+    trainers.add_to_back("test1");
+    trainers.remove_from_front();
+    trainers.print_names();
+    
     return 1;
 }
